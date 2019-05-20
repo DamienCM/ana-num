7412101fe1ea0601902fd20bb7f4fd6dee19af86
DamienCM <45606435+DamienCM@users.noreply.github.com>
2019-05-21 00:23:01 +0200

    Adding end of C
    Added question 8 question 9
    Still to go : comment for question 9

--- Projet_Analyse_Num/C.py
+++ Projet_Analyse_Num/C.py
@@ -6,17 +6,17 @@ import numpy as np
 import os
 
 if not os.path.isdir("outC"):
-    os.mkdir("outC")
+	os.mkdir("outC")
 
 
 # region question 1-2
 def g_ab(x, y):
-    global a, b
-    return np.sqrt(x ** 2 / a + y ** 2 / b)
+	global a, b
+	return np.sqrt(x ** 2 / a + y ** 2 / b)
 
 
 def h(x, y):
-    return np.cos(x) * np.sin(y)
+	return np.cos(x) * np.sin(y)
 
 
 fig_g = plt.figure()
@@ -78,19 +78,19 @@ fig_h.savefig('./outC/Question1-2h.png')
 
 # region question 3
 def grad_g_ab(x, y):
-    global a, b
-    return 2 * x / a, 2 * y / b
+	global a, b
+	return 2 * x / a, 2 * y / b
 
 
 def grad_h(x, y):
-    return -np.sin(x) * np.sin(y), np.cos(x) * np.cos(y)
+	return -np.sin(x) * np.sin(y), np.cos(x) * np.cos(y)
 
 
 def norme(V):
-    somme_quad = 0
-    for xi in V:
-        somme_quad += xi ** 2
-    return somme_quad ** 0.5
+	somme_quad = 0
+	for xi in V:
+		somme_quad += xi ** 2
+	return somme_quad ** 0.5
 
 
 # endregion question 3
@@ -106,8 +106,8 @@ P = [P0, P1, P2, P3]
 string = ""
 string += "Question 4" + '\n'
 for p in P:
-    string += "grad_g_ab" + str(p) + " = " + str(grad_g_ab(p[0], p[1])) + '\n'
-    string += "grad_h" + str(p) + " = " + str(grad_h(p[0], p[1])) + '\n'
+	string += "grad_g_ab" + str(p) + " = " + str(grad_g_ab(p[0], p[1])) + '\n'
+	string += "grad_h" + str(p) + " = " + str(grad_h(p[0], p[1])) + '\n'
 
 file = open('./outC/Question4.txt', 'w')
 file.write(string)
@@ -119,19 +119,19 @@ file.close()
 
 # region question 5
 def grad_pc(eps, m, u, xo, yo,
-            df):  # methode pas amelioree j'ai pas compris a quoi conrespondais les entrees
-    # je suppose df1,df2=df[0],df[1] = grad f
-    n = 0
-    X = [xo]
-    Y = [yo]
-    while norme(df(xo, yo)) > eps and n < m:
-        df1, df2 = df(xo, yo)
-        xo += df1 * u
-        yo += df2 * u
-        n += 1
-        X.append(xo)
-        Y.append(yo)
-    return n, np.array(X), np.array(Y)
+			df):  # methode pas amelioree j'ai pas compris a quoi conrespondais les entrees
+	# je suppose df1,df2=df[0],df[1] = grad f
+	n = 0
+	X = [xo]
+	Y = [yo]
+	while norme(df(xo, yo)) > eps and n < m:
+		df1, df2 = df(xo, yo)
+		xo += df1 * u
+		yo += df2 * u
+		n += 1
+		X.append(xo)
+		Y.append(yo)
+	return n, np.array(X), np.array(Y)
 
 
 # endregion question 5
@@ -159,8 +159,8 @@ XX_h, YY_h = np.meshgrid(X_h, Y_h)
 
 a, b = 2, 2 / 7
 
-Z_g = g_ab(XX_g,YY_g)
-Z_h = h(XX_h,YY_h)
+Z_g = g_ab(XX_g, YY_g)
+Z_h = h(XX_h, YY_h)
 
 # Lignes de Champ
 ax_g.contour(XX_g, YY_g, Z_g, 30, zdir='z', cmap=cm.coolwarm)  # 30 = nombre de lignes de champs
@@ -197,10 +197,12 @@ fig_h.legend()
 
 fig_g.savefig('./outC/Q6g.png')
 fig_h.savefig('./outC/Q6h.png')
-file = open('./outC/Question6.txt','w')
-file.write("Pour h on remarque que la methode gradient pas constant converge vers le sommet assez rapidement (n=" +str(n_g) + ") comme prevu \n"
-           "Cependant pour g_ab puisque le point de depart est (0,0) la methode s'arrete instantanement (n=" +str(n_g) + ") alors que le point n'est pas au 'sommet'\n"
-           "C'est normal, il s'agit d'un point d'equilibre instable grad_g_ab(0,0)=0,0")
+file = open('./outC/Question6.txt', 'w')
+file.write("Pour h on remarque que la methode gradient pas constant converge vers le sommet assez rapidement (n=" + str(
+	n_g) + ") comme prevu \n"
+		   "Cependant pour g_ab puisque le point de depart est (0,0) la methode s'arrete instantanement (n=" + str(
+	n_g) + ") alors que le point n'est pas au 'sommet'\n"
+		   "C'est normal, il s'agit d'un point d'equilibre instable grad_g_ab(0,0)=0,0")
 file.close()
 # endregion question 6
 
@@ -218,18 +220,102 @@ xo, yo = (-4, 4)
 
 U = np.linspace(-0.99, -0.001, 500)
 for u in U:
-    _, X, Y = grad_pc(eps, m, u, xo, yo, grad_g_ab)
-    Z = g_ab(X, Y)
-    ecarts.append(((X[-1] - 0) ** 2 + (Y[-1] - 0) ** 2) ** 0.5)
+	_, X, Y = grad_pc(eps, m, u, xo, yo, grad_g_ab)
+	Z = g_ab(X, Y)
+	ecarts.append(((X[-1] - 0) ** 2 + (Y[-1] - 0) ** 2) ** 0.5)
 
 ax_q7.plot(U, ecarts)
 ax_q7.grid()
 fig_q7.suptitle("Ecarts en fonction de u")
 fig_q7.savefig('./outC/Q7.png')
-file = open('./outC/Question7.txt','w')
+file = open('./outC/Question7.txt', 'w')
 file.write("Interpretation : \n"
-           "Un pas trop faible implique que l'on peut ne pas forcement arriver à la solution avant les 120 iterations \n"
-           "Avec un pas trop grand on 'jongle' autour de la solution sans jamais sans rapprocher suffisament \n"
-           )
+		   "Un pas trop faible implique que l'on peut ne pas forcement arriver à la solution avant les 120 iterations \n"
+		   "Avec un pas trop grand on 'jongle' autour de la solution sans jamais sans rapprocher suffisament \n"
+		   )
 file.close()
+
+
 # endregion question 7
+
+
+# region question 8
+def gradamax(eps, m, u, xo, yo, f, df):
+	n = 0
+	x, y = xo, yo
+	while n < m and norme(df(x, y)) > eps:
+		k = 1
+		dfx, dfy = df(x, y)
+		F1 = f(x + k * u * dfx, y + k * u * dfy)
+		F2 = f(x + (k + 1) * u * dfx, y + (k + 1) * u * dfy)
+		while F2 >= F1 and k < m:
+			F1 = f(x + k * u * dfx, y + k * u * dfy)
+			F2 = f(x + (k + 1) * u * dfx, y + (k + 1) * u * dfy)
+			k += 1
+		x += k * u * dfx
+		y += k * u * dfy
+		n += 1
+	return n, x, y
+
+
+def gradamin(eps, m, u, xo, yo, f, df):
+	n = 0
+	x, y = xo, yo
+	while n < m and norme(df(x, y)) > eps:
+		k = 1
+		dfx, dfy = df(x, y)
+		F1 = f(x - k * u * dfx, y - k * u * dfy)
+		F2 = f(x - (k + 1) * u * dfx, y - (k + 1) * u * dfy)
+		while F2 <= F1 and k < m:
+			F1 = f(x - k * u * dfx, y - k * u * dfy)
+			F2 = f(x - (k + 1) * u * dfx, y - (k + 1) * u * dfy)
+			k += 1
+		x = x - k * u * dfx
+		y = y - k * u * dfy
+		n += 1
+	return n, x, y
+
+
+file = open('./outC/Question8.txt', 'w')
+file.write(
+	"gradamax(0.1, 50, 0.1, 1, 0, g_ab, grad_g_ab) = " + str(gradamax(0.1, 50, 0.1, 1, 0, g_ab, grad_g_ab)) + "\n")
+file.write("gradamax(0.1, 50, 0.1, 0, 0, h, grad_h) = " + str(gradamax(0.1, 50, 0.1, 0, 0, h, grad_h)) + "\n")
+
+file.write(
+	"gradamin(0.001, 100, 0.01, 1, 5, g_ab, grad_g_ab) = " + str(
+		gradamin(0.001, 100, 0.01, 1, 5, g_ab, grad_g_ab)) + "\n")
+file.write(
+	"gradamin(0.001, 100, 0.01, 0, 0, h, grad_h) = " + str(gradamin(0.001, 100, 0.01, 0, 0, h, grad_h)) + "\n")
+file.close()
+# endregion question 8
+
+
+# region Question 9
+a, b = 1, 20
+xo, yo = -4, 4  # on part du point suivant
+
+U = np.linspace(-0.999, 0.001, 10 ** 3)
+eps = 10 ** -5
+ecarts_grada = []
+ecarts_gradpc = []
+n_max = 10000
+
+for u in U:
+	ecarts_grada.append(gradamin(eps, n_max, -u, xo, yo, g_ab, grad_g_ab)[0])
+	ecarts_gradpc.append(grad_pc(eps, n_max, u, xo, yo, grad_g_ab)[0])
+
+fig_q9 = plt.figure()
+ax_q9 = fig_q9.gca()
+
+ax_q9.plot(U, ecarts_gradpc, label="Ecarts grad_pc")
+ax_q9.plot(U, ecarts_grada, label="Ecarts grada")
+fig_q9.suptitle("Nombre d'itérations en fonction de u")
+ax_q9.grid()
+ax_q9.set_xlabel('u')
+ax_q9.set_ylabel('n')
+fig_q9.legend()
+fig_q9.savefig("./outC/Q9.png")
+ax_q9.set_yscale('log')
+fig_q9.savefig('./outC/Q9bis.png')
+
+# endregion Question 9
