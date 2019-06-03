7934fd14ba37c4081296b5e66f95bbf4df43424b
ABRINI <mouad.abrini@gmail.com>
2019-06-03 23:14:18 +0200

    ajout E

--- Projet_Analyse_Num/D.py
+++ Projet_Analyse_Num/D.py
@@ -1,22 +1,22 @@
 import numpy as np
 
 
-def G(A,y0, b):
+def G(A, y0, b):
     return 2 * (A * y0 - b)
 
 
-
-
 def NormeMatrice(M):
-    taille=M.shape[0]
+    taille = M.shape[0]
     s = 0
     for i in range(taille):
         for j in range(taille):
-            s = s + M[i][j]**2
+            s = s + M[i][j] ** 2
     return np.sqrt(s)
+
 
 def norme(M):
     return np.linalg.norm(M)
+
 
 def nozero(M):
     taille = M.shape[0]
@@ -27,34 +27,18 @@ def nozero(M):
     return True
 
 
-
-def phi(A,y,b):
-    return y.T * A * y - 2 * y.T * b
-
-
-
-
-def gradopt(A,b,y0,eps,m):
+def gradopt(A, b, y0, eps, m):
     n = 0
-    g = 2*(A*y0-b)
-    ro=(norme(g)**2)/(2*(g.transpose())*A*g)
-    y1 = y0 - ro*g
-    while(n<m and norme(y1-y0)>=eps):
-        y0=y1
-        g=2*(A*y0-b)
-        if(nozero(g)):
-            ro = ro=(norme(g)**2)/(2*g.T*A*g)
+    g = 2 * (A * y0 - b)
+    ro = (norme(g) ** 2) / (2 * g.T * A * g)
+    y1 = y0 - ro * g
+    while n < m and norme(y1 - y0) >= eps:
+        y0 = y1
+        g = 2 * (A * y0 - b)
+        if nozero(g):
+            ro = ro = (norme(g) ** 2) / (2 * g.T * A * g)
         else:
-            ro=0
-        y1=y0-ro*g
+            ro = 0
+        y1 = y0 - ro * g
         n = n + 1
     return y1
-
-print(gradopt(np.array([[2,1],[1,2]]),np.array([1,2]),np.array([0,1]),0.0000001,10))
-
-
-
-
-
-
-
---
+++ Projet_Analyse_Num/E1.py
@@ -0,0 +1,60 @@
+
+import numpy as np
+N=10
+L=1
+dx=L/(N+1)
+fo=500
+fn=350
+eps=0.0001
+c=10
+A=2*np.eye(N)
+u=-np.ones(N)
+U=np.diag(u,1)
+V=np.diag(u,-1)
+A=A+V+U
+A=A*(1/(dx**2))
+b=[]
+f=3000
+for t in range(1,N+1):
+  if(t==1):
+    b=np.array([b,(fo/(dx**2))+f])
+  elif(t==N):
+    b=np.array([b,(fn/(dx**2))+f])
+  else:
+    b=np.array([b,f])
+
+yo=np.zeros(N)
+m=100
+
+yk=np.zeros(N)
+i=0
+ord=[]
+
+def norme(M):
+    return np.linalg.norm(M)
+
+def nozero(M):
+    taille = M.shape[0]
+    for i in range(taille):
+        for j in range(taille):
+            if M[i][j] == 0:
+                return False
+    return True
+
+def gradopt(A,b,y0,eps,m):
+    n = 0
+    g = 2*(A*y0-b)
+    ro=(norme(g)**2)/(2*g.T*A*g)
+    y1 = y0 - ro*g
+    while(n<m and norme(y1-y0)>=eps):
+        y0=y1
+        g=2*(A*y0-b)
+        if(nozero(g)):
+            ro = ro=(norme(g)**2)/(2*g.T*A*g)
+        else:
+            ro=0
+        y1=y0-ro*g
+        n = n + 1
+    return y1
+yy=gradopt(A,b,yo,eps,m)
+print(yy)
